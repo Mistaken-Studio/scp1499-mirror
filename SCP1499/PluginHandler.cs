5207f9a06f741daebbf5d8f1d75b9ac7b8bb4744
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-07-18 12:25:40 +0200

    Initial

---
+++ SCP1499/PluginHandler.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+namespace Mistaken.SCP1499
+{
+    /// <inheritdoc/>
+    public class PluginHandler : Plugin<Config, Translation>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Mistaken Devs";
+
+        /// <inheritdoc/>
+        public override string Name => "SCP1499";
+
+        /// <inheritdoc/>
+        public override string Prefix => "M1499";
+
+        /// <inheritdoc/>
+        public override PluginPriority Priority => PluginPriority.Higher;
+
+        /// <inheritdoc/>
+        public override Version RequiredExiledVersion => new Version(2, 11, 0);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            new SCP1499Handler(this);
+
+            API.Diagnostics.Module.OnEnable(this);
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            API.Diagnostics.Module.OnDisable(this);
+
+            base.OnDisabled();
+        }
+
+        internal static PluginHandler Instance { get; private set; }
+
+        internal static bool VerbouseOutput => Instance.Config.VerbouseOutput;
+    }
+}
